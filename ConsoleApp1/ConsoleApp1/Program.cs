d01183f8525ae53a9d18e9d2e297fb26bc1f5d14
Jigar1992 <jigar.thakkar@tatvasoft.com>
2023-03-08 21:30:10 +0530

    Commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System; //Namespace
+
 Console.WriteLine("Hello, World!");
+
+//var a = 30;
+
+//Console.WriteLine(a);
+
+int varInteger = 10;
+long varLong = 605;
+float varFloat = 50.123456f;
+double varDouble = 51.2554;
+bool varBool = false;
+char varChar = 'B';
+string varString = "Jigar Thakkar";
+
+// Implicit type : 
+
+//double varDoubleImplicit = varInteger; /// Implicit type
+
+//Console.WriteLine(varDoubleImplicit);
+
+
+int varIntDouble = (int)varDouble;
+int varIntDouble1 = Convert.ToInt32(varDouble);
+
+//Console.WriteLine(varIntDouble); /// Explicit type casting
+//Console.WriteLine("\n"); /// Explicit type casting
+//Console.WriteLine(varIntDouble1); /// Explicit type casting
+//var a = 30;
+
+var b = varInteger + varLong;
+
+int a = 5;
+Console.WriteLine("a++ :" + a++);
+Console.WriteLine("++a :" + ++a);
+Console.WriteLine("a :" + a);
+
+Console.WriteLine("\n");
+
+int y = 5;
+Console.WriteLine("y++ :" + y++);
+Console.WriteLine("++y :" + ++y);
+
+//var a = 30;
